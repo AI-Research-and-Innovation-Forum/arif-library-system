994462b2cddb179e2df6650e592290dbaea79878
Soham156 <145569857+Soham156@users.noreply.github.com>
2025-07-07 00:12:03 +0530

    Update uploadMiddleware.js

--- server/middlewares/uploadMiddleware.js
+++ server/middlewares/uploadMiddleware.js
@@ -1,9 +1,14 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    const uploadDir = 'uploads';
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
